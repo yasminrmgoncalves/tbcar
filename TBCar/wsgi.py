891a6eb4a5a7a58b43b41c0901d2ae68ae1316cb
Yasmin Goncalves <yasminrmgoncalves@gmail.com>
2023-06-15 22:10:31 -0300

    update wsgi.py

--- TBCar/wsgi.py
+++ TBCar/wsgi.py
@@ -11,7 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TBCar.settings')
 
